e5f994e28713285ac428ef28f0553c033c1f2ca5
Avic <Avicennasis@gmail.com>
2015-08-06 07:53:44 -0400

    Update avicbotrdvoyage.sh
    update paths for Tool Labs

--- bin/avicbotrdvoyage.sh
+++ bin/avicbotrdvoyage.sh
@@ -8,53 +8,53 @@ PYTHONPATH=/shared/pywikipedia/core:/shared/pywikipedia/core/externals/httplib2:
 
 if [ $1 == "en" ] || [ $1 == "es" ] || [ $1 == "fa" ] || [ $1 == "ru" ] ; then
 # Try a full scan first
-python pwb.py redirect -fullscan do -family:wikivoyage -always -lang:$1 -namespace:828
-python pwb.py redirect -fullscan do -family:wikivoyage -always -lang:$1 -namespace:15
-python pwb.py redirect -fullscan do -family:wikivoyage -always -lang:$1 -namespace:14
-python pwb.py redirect -fullscan do -family:wikivoyage -always -lang:$1 -namespace:13
-python pwb.py redirect -fullscan do -family:wikivoyage -always -lang:$1 -namespace:12
-python pwb.py redirect -fullscan do -family:wikivoyage -always -lang:$1 -namespace:11
-python pwb.py redirect -fullscan do -family:wikivoyage -always -lang:$1 -namespace:10
-python pwb.py redirect -fullscan do -family:wikivoyage -always -lang:$1 -namespace:9
-python pwb.py redirect -fullscan do -family:wikivoyage -always -lang:$1 -namespace:8
-python pwb.py redirect -fullscan do -family:wikivoyage -always -lang:$1 -namespace:7
-python pwb.py redirect -fullscan do -family:wikivoyage -always -lang:$1 -namespace:6
-python pwb.py redirect -fullscan do -family:wikivoyage -always -lang:$1 -namespace:5
-python pwb.py redirect -fullscan do -family:wikivoyage -always -lang:$1 -namespace:4
-python pwb.py redirect -fullscan do -family:wikivoyage -always -lang:$1 -namespace:3
-python pwb.py redirect -fullscan do -family:wikivoyage -always -lang:$1 -namespace:2
-python pwb.py redirect -fullscan do -family:wikivoyage -always -lang:$1 -namespace:1
-python pwb.py redirect -fullscan do -family:wikivoyage -always -lang:$1 -namespace:0
+python /shared/pywikipedia/core/scripts/redirect.py -fullscan do -family:wikivoyage -always -lang:$1 -namespace:828
+python /shared/pywikipedia/core/scripts/redirect.py -fullscan do -family:wikivoyage -always -lang:$1 -namespace:15
+python /shared/pywikipedia/core/scripts/redirect.py -fullscan do -family:wikivoyage -always -lang:$1 -namespace:14
+python /shared/pywikipedia/core/scripts/redirect.py -fullscan do -family:wikivoyage -always -lang:$1 -namespace:13
+python /shared/pywikipedia/core/scripts/redirect.py -fullscan do -family:wikivoyage -always -lang:$1 -namespace:12
+python /shared/pywikipedia/core/scripts/redirect.py -fullscan do -family:wikivoyage -always -lang:$1 -namespace:11
+python /shared/pywikipedia/core/scripts/redirect.py -fullscan do -family:wikivoyage -always -lang:$1 -namespace:10
+python /shared/pywikipedia/core/scripts/redirect.py -fullscan do -family:wikivoyage -always -lang:$1 -namespace:9
+python /shared/pywikipedia/core/scripts/redirect.py -fullscan do -family:wikivoyage -always -lang:$1 -namespace:8
+python /shared/pywikipedia/core/scripts/redirect.py -fullscan do -family:wikivoyage -always -lang:$1 -namespace:7
+python /shared/pywikipedia/core/scripts/redirect.py -fullscan do -family:wikivoyage -always -lang:$1 -namespace:6
+python /shared/pywikipedia/core/scripts/redirect.py -fullscan do -family:wikivoyage -always -lang:$1 -namespace:5
+python /shared/pywikipedia/core/scripts/redirect.py -fullscan do -family:wikivoyage -always -lang:$1 -namespace:4
+python /shared/pywikipedia/core/scripts/redirect.py -fullscan do -family:wikivoyage -always -lang:$1 -namespace:3
+python /shared/pywikipedia/core/scripts/redirect.py -fullscan do -family:wikivoyage -always -lang:$1 -namespace:2
+python /shared/pywikipedia/core/scripts/redirect.py -fullscan do -family:wikivoyage -always -lang:$1 -namespace:1
+python /shared/pywikipedia/core/scripts/redirect.py -fullscan do -family:wikivoyage -always -lang:$1 -namespace:0
 # Pull from special page, in case full scan fails
-python pwb.py redirect do -family:wikivoyage -always -lang:$1 -namespace:828
-python pwb.py redirect do -family:wikivoyage -always -lang:$1 -namespace:15
-python pwb.py redirect do -family:wikivoyage -always -lang:$1 -namespace:14
-python pwb.py redirect do -family:wikivoyage -always -lang:$1 -namespace:13
-python pwb.py redirect do -family:wikivoyage -always -lang:$1 -namespace:12
-python pwb.py redirect do -family:wikivoyage -always -lang:$1 -namespace:11
-python pwb.py redirect do -family:wikivoyage -always -lang:$1 -namespace:10
-python pwb.py redirect do -family:wikivoyage -always -lang:$1 -namespace:9
-python pwb.py redirect do -family:wikivoyage -always -lang:$1 -namespace:8
-python pwb.py redirect do -family:wikivoyage -always -lang:$1 -namespace:7
-python pwb.py redirect do -family:wikivoyage -always -lang:$1 -namespace:6
-python pwb.py redirect do -family:wikivoyage -always -lang:$1 -namespace:5
-python pwb.py redirect do -family:wikivoyage -always -lang:$1 -namespace:4
-python pwb.py redirect do -family:wikivoyage -always -lang:$1 -namespace:3
-python pwb.py redirect do -family:wikivoyage -always -lang:$1 -namespace:2
-python pwb.py redirect do -family:wikivoyage -always -lang:$1 -namespace:1
-python pwb.py redirect do -family:wikivoyage -always -lang:$1 -namespace:0
+python /shared/pywikipedia/core/scripts/redirect.py do -family:wikivoyage -always -lang:$1 -namespace:828
+python /shared/pywikipedia/core/scripts/redirect.py do -family:wikivoyage -always -lang:$1 -namespace:15
+python /shared/pywikipedia/core/scripts/redirect.py do -family:wikivoyage -always -lang:$1 -namespace:14
+python /shared/pywikipedia/core/scripts/redirect.py do -family:wikivoyage -always -lang:$1 -namespace:13
+python /shared/pywikipedia/core/scripts/redirect.py do -family:wikivoyage -always -lang:$1 -namespace:12
+python /shared/pywikipedia/core/scripts/redirect.py do -family:wikivoyage -always -lang:$1 -namespace:11
+python /shared/pywikipedia/core/scripts/redirect.py do -family:wikivoyage -always -lang:$1 -namespace:10
+python /shared/pywikipedia/core/scripts/redirect.py do -family:wikivoyage -always -lang:$1 -namespace:9
+python /shared/pywikipedia/core/scripts/redirect.py do -family:wikivoyage -always -lang:$1 -namespace:8
+python /shared/pywikipedia/core/scripts/redirect.py do -family:wikivoyage -always -lang:$1 -namespace:7
+python /shared/pywikipedia/core/scripts/redirect.py do -family:wikivoyage -always -lang:$1 -namespace:6
+python /shared/pywikipedia/core/scripts/redirect.py do -family:wikivoyage -always -lang:$1 -namespace:5
+python /shared/pywikipedia/core/scripts/redirect.py do -family:wikivoyage -always -lang:$1 -namespace:4
+python /shared/pywikipedia/core/scripts/redirect.py do -family:wikivoyage -always -lang:$1 -namespace:3
+python /shared/pywikipedia/core/scripts/redirect.py do -family:wikivoyage -always -lang:$1 -namespace:2
+python /shared/pywikipedia/core/scripts/redirect.py do -family:wikivoyage -always -lang:$1 -namespace:1
+python /shared/pywikipedia/core/scripts/redirect.py do -family:wikivoyage -always -lang:$1 -namespace:0
 fi
 
 if [ $1 == "en" ] || [ $1 == "es" ]  ; then
 # Try a full scan first
-python pwb.py redirect -fullscan do -family:wikivoyage -always -lang:$1 -namespace:829
+python /shared/pywikipedia/core/scripts/redirect.py -fullscan do -family:wikivoyage -always -lang:$1 -namespace:829
 # Pull from special page, in case full scan fails
-python pwb.py redirect do -family:wikivoyage -always -lang:$1 -namespace:829
+python /shared/pywikipedia/core/scripts/redirect.py do -family:wikivoyage -always -lang:$1 -namespace:829
 fi
 
 if [ $1 == "fa" ] ; then
 # Try a full scan first
-python pwb.py redirect -fullscan do -family:wikivoyage -always -lang:$1 -namespace:106
+python /shared/pywikipedia/core/scripts/redirect.py -fullscan do -family:wikivoyage -always -lang:$1 -namespace:106
 # Pull from special page, in case full scan fails
-python pwb.py redirect do -family:wikivoyage -always -lang:$1 -namespace:106
+python /shared/pywikipedia/core/scripts/redirect.py do -family:wikivoyage -always -lang:$1 -namespace:106
 fi
