c1a4ec5e1c728504fbe000ec06dc1062a5dfbfb4
Avic <Avicennasis@gmail.com>
2015-08-06 07:42:40 -0400

    Create avicbotrdvoyageall.sh
    Companion script for cron

---
+++ bin/avicbotrdvoyageall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#This is our cron script for redirects 
+#Paths are formatted for Tool Labs
+
+date >>~/logs/rdvoyage.log
+~/.pywikibot/avicbotrdvoyage.sh en
+echo "en done" >>~/logs/rdvoyage.log
+~/.pywikibot/avicbotrdvoyage.sh es
+echo "es done" >>~/logs/rdvoyage.log
+~/.pywikibot/avicbotrdvoyage.sh fa
+echo "fa done" >>~/logs/rdvoyage.log
+~/.pywikibot/avicbotrdvoyage.sh ru
+echo "ru done" >>~/logs/rdvoyage.log
+date >>~/logs/rdvoyage.log
