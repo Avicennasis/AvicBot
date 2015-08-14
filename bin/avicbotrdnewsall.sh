57833018b8b1b1f423d72d3e5183e13ad4ef71f6
Avic <Avicennasis@gmail.com>
2015-08-14 06:42:06 -0400

    Create avicbotrdnewsall.sh
    create news cron script

---
+++ bin/avicbotrdnewsall.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+date >>~/logs/rdnews.log
+~/.pywikibot/avicbotrdnews.sh ar
+echo "ar done" >>~/logs/rdnews.log
+~/.pywikibot/avicbotrdnews.sh bg
+echo "bg done" >>~/logs/rdnews.log
+~/.pywikibot/avicbotrdnews.sh bs
+echo "bs done" >>~/logs/rdnews.log
+~/.pywikibot/avicbotrdnews.sh ca
+echo "ca done" >>~/logs/rdnews.log
+~/.pywikibot/avicbotrdnews.sh cs
+echo "cs done" >>~/logs/rdnews.log
+~/.pywikibot/avicbotrdnews.sh el
+echo "el done" >>~/logs/rdnews.log
+~/.pywikibot/avicbotrdnews.sh en
+echo "en done" >>~/logs/rdnews.log
+~/.pywikibot/avicbotrdnews.sh eo
+echo "eo done" >>~/logs/rdnews.log
+~/.pywikibot/avicbotrdnews.sh fa
+echo "fa done" >>~/logs/rdnews.log
+~/.pywikibot/avicbotrdnews.sh fi
+echo "fi done" >>~/logs/rdnews.log
+~/.pywikibot/avicbotrdnews.sh he
+echo "he done" >>~/logs/rdnews.log
+~/.pywikibot/avicbotrdnews.sh ja
+echo "ja done" >>~/logs/rdnews.log
+~/.pywikibot/avicbotrdnews.sh ko
+echo "ko done" >>~/logs/rdnews.log
+~/.pywikibot/avicbotrdnews.sh no
+echo "no done" >>~/logs/rdnews.log
+~/.pywikibot/avicbotrdnews.sh pt
+echo "pt done" >>~/logs/rdnews.log
+~/.pywikibot/avicbotrdnews.sh ro
+echo "ro done" >>~/logs/rdnews.log
+~/.pywikibot/avicbotrdnews.sh ru
+echo "ru done" >>~/logs/rdnews.log
+~/.pywikibot/avicbotrdnews.sh sq
+echo "sq done" >>~/logs/rdnews.log
+~/.pywikibot/avicbotrdnews.sh sr
+echo "sr done" >>~/logs/rdnews.log
+~/.pywikibot/avicbotrdnews.sh sv
+echo "sv done" >>~/logs/rdnews.log
+~/.pywikibot/avicbotrdnews.sh ta
+echo "ta done" >>~/logs/rdnews.log
+~/.pywikibot/avicbotrdnews.sh tr
+echo "tr done" >>~/logs/rdnews.log
+~/.pywikibot/avicbotrdnews.sh uk
+echo "uk done" >>~/logs/rdnews.log
+~/.pywikibot/avicbotrdnews.sh zh
+echo "zh done" >>~/logs/rdnews.log
+date >>~/logs/rdnews.log
+
