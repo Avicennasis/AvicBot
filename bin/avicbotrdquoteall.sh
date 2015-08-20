fb533b824ce45730f5cf3f78f3ceacc3d363aa77
Avic <Avicennasis@gmail.com>
2015-08-20 07:37:45 -0400

    Create avicbotrdquoteall.sh

---
+++ bin/avicbotrdquoteall.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+# This is our cron script for redirects 
+# Paths are formatted for Tool Labs
+
+date >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh af
+echo "af done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh ar
+echo "ar done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh az
+echo "az done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh be
+echo "be done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh bg
+echo "bg done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh br
+echo "br done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh bs
+echo "bs done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh ca
+echo "ca done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh cs
+echo "cs done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh cy
+echo "cy done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh da
+echo "da done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh el
+echo "el done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh eo
+echo "eo done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh es
+echo "es done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh et
+echo "et done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh eu
+echo "eu done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh fa
+echo "fa done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh fi
+echo "fi done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh fr
+echo "fr done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh gl
+echo "gl done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh gu
+echo "gu done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh he
+echo "he done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh hi
+echo "hi done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh hu
+echo "hu done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh hy
+echo "hy done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh id
+echo "id done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh is
+echo "is done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh it
+echo "it done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh ja
+echo "ja done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh ka
+echo "ka done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh kn
+echo "kn done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh ko
+echo "ko done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh ku
+echo "ku done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh ky
+echo "ky done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh la
+echo "la done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh li
+echo "li done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh lt
+echo "lt done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh ml
+echo "ml done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh mr
+echo "mr done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh nl
+echo "nl done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh nn
+echo "nn done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh no
+echo "no done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh pt
+echo "pt done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh ro
+echo "ro done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh ru
+echo "ru done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh sk
+echo "sk done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh sl
+echo "sl done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh sq
+echo "sq done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh sr
+echo "sr done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh su
+echo "su done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh sv
+echo "sv done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh ta
+echo "ta done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh te
+echo "te done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh tr
+echo "tr done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh uk
+echo "uk done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh ur
+echo "ur done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh uz
+echo "uz done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh vi
+echo "vi done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh wo
+echo "wo done" >>~/logs/rdquotes.log
+~/.pywikibot/avicbotrdquotes.sh zh
+echo "zh done" >>~/logs/rdquotes.log
+date >>~/logs/rdquotes.log
+
