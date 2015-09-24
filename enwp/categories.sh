b5449d551f236d1839c0be0a82d40e93b84a1d8c
Avic <Avicennasis@gmail.com>
2015-09-24 09:39:04 -0400

    Update categories.sh
    Add Category:Misplaced_userspace_drafts

--- enwp/categories.sh
+++ enwp/categories.sh
@@ -6,6 +6,7 @@ PYTHONPATH=/shared/pywikipedia/core:/shared/pywikipedia/core/externals/httplib2:
 python /shared/pywikipedia/core/scripts/category.py -family:wikipedia -lang:en listify -from:Wikipedians_who_are_indefinitely_blocked_for_link-spamming -to:User:AvicBot/linkspam -overwrite
 python /shared/pywikipedia/core/scripts/category.py -family:wikipedia -lang:en listify -from:Wikipedians_who_are_indefinitely_blocked_for_spamming -to:User:AvicBot/spam -overwrite
 python /shared/pywikipedia/core/scripts/category.py -family:wikipedia -lang:en listify -from:Wikipedians_who_are_indefinitely_blocked_for_advertising -to:User:AvicBot/advertising -overwrite
+python /shared/pywikipedia/core/scripts/category.py -family:wikipedia -lang:en listify -from:Misplaced_userspace_drafts -to:User:AvicBot/drafts2 -overwrite
 
 # These are too large to be used with listify currently
 # python /shared/pywikipedia/core/scripts/category.py -family:wikipedia -lang:en listify -from:Wikipedians_who_are_indefinitely_blocked_for_a_violation_of_the_username_policy -to:User:AvicBot/upolvio -overwrite
