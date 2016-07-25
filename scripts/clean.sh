6dc52b73f435fe38786ca41a59a2957e417ba3ff
Avic <Avicennasis@gmail.com>
2016-07-24 23:49:35 -0400

    Create clean.sh
    cleanup script

---
+++ scripts/clean.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# while true; do
+
+rm ~/logs/*.del
+# rm ~/*.out
+rm ~/service.manifest
+rm ~/service.log
+rm ~/error.log
+rm ~/access.log
+rm ~/uaa*.out
+rm ~/uaa*.err
+rm ~/clean.out
+rm ~/clean.err
+# rm ~/commonsrd.err
+rm ~/metard.err
+rm ~/commonscat.out
+rm ~/mysandboxes.err
+rm ~/avicbotrdsourceall.err
+rm ~/categories.err
+rm ~/rdallsource.err
+rm ~/login.err
+rm ~/rdallvoy.err
+# rm ~/avicbotirc.err
+# rm ~avicbotirc.out
+rm ~/avicbotirc2.err
+rm ~/avicbotirc2.out
+rm ~/avicbotrdnewsall.err
+rm ~/categories.out
+rm ~/commonsrd.out
+rm ~/csd.err
+rm ~/csd.out
+rm ~/enorphans.out
+
+
+
+
+# done
